7da0da7c39949ac127e61e6fa996ea9050bb9e20
stepankarshin <godownforway@gmail.com>
2024-05-29 23:58:26 +0300

    new function!!!

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def upp_text(text):
     return text.upper()
+
+
+def upp_first_letters(text):
+    for i in range(len(text)):
+        if text[i - 1] == ' ':
+            text[i] = text[i].upper()
+        text[0] = text[0].upper()
+        return text
